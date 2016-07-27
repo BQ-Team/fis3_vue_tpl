cd211a774ee8e3540d3202770e86ba24730e7813
zyc <501360128@qq.com>
2016-07-27 21:45:18 +0800

    vue start

---
+++ modules/pages/order/page.js
@@ -0,0 +1,30 @@
+/**
+ * Created by zyc on 2016/6/11.
+ */
+
+define(function (require, exports, module) {
+    var basePage = require('basePage');
+    var util = require("util");
+    module.exports = Vue.extend({
+        mixins: [basePage],
+        html: $(__inline("./page.html")),
+        title: "订单",
+        //数据
+        data: {},
+        //组件init 仅执行一次
+        ready: function (params) {
+            //事件初始化
+            this.initEvent();
+            this.changeEvent();
+        },
+        //每次切换进入到该组件 都会被执行
+        load: function (params) {
+            util.logger.log(this.title, ' load');
+        },
+        /** 页面跳转
+         * */
+        initEvent: function () {
+            util.logger.log(this.title, 'ready , 事件初始化');
+        },
+    })
+});
