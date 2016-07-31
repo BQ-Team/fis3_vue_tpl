e42993718853a3603d8a05570ca782886229dcb8
zyc <501360128@qq.com>
2016-07-31 20:18:19 +0800

    add something

--- modules/pages/detail/page.js
+++ modules/pages/detail/page.js
@@ -11,22 +11,41 @@ define(function (require, exports, module) {
         template: __inline("./page.html"),
         data: function () {
             return {
-
+                cityCode: "100002",
+                dateTime: "",
+                inputMoney:100.12
+            }
+        },
+        filters:{
+            'cityFormat': function (value) {
+                var store = require("store");   //引用
+                var item = store.cityData.filter(function (city) {
+                    return city.code == value;
+                });
+                return item ? item[0].name : ""; //三元
             }
         },
         ready: function () {
 
+
+
         },
         attached: function () {
+            util.logger.log(this.title + " 進入,參數", this.params);
 
-            util.logger.log(this.title+" 進入,參數", this.params);
         },
         detached: function () {
 
         },
         methods: {
-            showMsg: function () {
-
+            showSelectCity: function () {
+                var self = this;
+                this.showDialog("pages/selectCity", {
+                    city: self.cityCode,
+                    ok: function (newCity) {
+                        self.cityCode = newCity;
+                    }
+                }, "bottom");
             }
         }
     });
